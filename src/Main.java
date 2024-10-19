982c570997c9a9e7ed81b8287a891bfbf9ff790b
Miguel Castillo <miguel.castillo.valero@gmail.com>
2024-10-19 00:27:24 -0500

    Codigo validador en consola

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        System.out.println("Iniciando Validador de Tarjetas");
+        System.out.println("Ingrese numero de Tarjeta");
+        String Tarjeta =  key.nextLine();
+
+        validador val = new validador();
+
+        if(val.validarTarjeta(Tarjeta) == true){
+            System.out.println("La tarjeta es valida");
+        }else{
+            System.out.println("la tarjeta ingresada no es valida");
+        }
+
+    }
+}
---
+++ src/validador.java
@@ -0,0 +1,46 @@
+public class validador {
+
+    validador() {
+
+    }
+
+    public boolean validarTarjeta(String cardNumber) {
+
+
+        int digitos = cardNumber.length();
+        int sum = 0;
+        boolean esSegundo = false;
+
+
+        for (int i = digitos - 1; i >= 0; i--) {
+            int d = cardNumber.charAt(i) - '0';
+            System.out.println("\nProcesando dígito en la posición " + (i + 1) + ": " + d);
+
+            if (esSegundo) {
+                d = d * 2;
+                System.out.println("El dígito " + cardNumber.charAt(i) + " fue duplicado a " + d);
+            }
+
+            if (d > 9) {
+                System.out.println("El dígito duplicado es mayor que 9, sumando los dígitos de " + d);
+                sum += d / 10;
+                sum += d % 10;
+                System.out.println("Suma actual: " + sum);
+            } else {
+
+                sum += d;
+                System.out.println("El dígito sumado es menor o igual a 9, suma actual: " + sum);
+            }
+
+            esSegundo = !esSegundo;
+        }
+
+        System.out.println("\nSuma total final: " + sum);
+
+        if (sum % 10 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
